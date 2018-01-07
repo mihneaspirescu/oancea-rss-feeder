43ad4ab7eb36f5af7a39d0bb9eeb68e8e0af14a1
Mihnea Spirescu <mihnea.spirescu@gmail.com>
2018-01-07 17:35:13 +0000

    trying something new so save this bastard

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import hashlib
+
+from rfeed import *
 
 try:
     from urllib.parse import urlparse
@@ -15,7 +18,13 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 # all news DB
-allnews = []
+allnews = {}
+
+
+def merge_two_dicts(x, y):
+    z = x.copy()  # start with x's keys and values
+    z.update(y)  # modifies z with y's keys and values & returns None
+    return z
 
 
 def captureContent(tr):
@@ -31,24 +40,30 @@ def captureContent(tr):
 
 
 def captureText(tables):
-    news11 = []
-    news13 = []
+    news11 = {}
+    news13 = {}
     trs11 = tables[11].findAll('tr')
     trs13 = tables[13].findAll('tr')
 
     for tr in trs13:
         titles, content, link = captureContent(tr)
         if len(titles) != 0 and len(content) != 0:
-            news13.append((titles[0], content, link.get('href')))
+            hash_object = hashlib.md5((str(titles[0]) + str(content) + str(link.get('href'))).encode())
+            news13[hash_object.hexdigest()] = (titles[0], content, link.get('href'))
 
     for tr in trs11:
         titles, content, link = captureContent(tr)
         if len(titles) != 0 and len(content) != 0:
-            news11.append((titles[0], content, link.get('href')))
+            hash_object = hashlib.md5((str(titles[0]) + str(content) + str(link.get('href'))).encode())
+            print(hash_object.hexdigest())
+
+            news11[hash_object.hexdigest()] = (titles[0], content, link.get('href'))
 
     if len(news11) != 0:
+        print(news11.items())
         return news11
     else:
+        print(news13.items())
         return news13
 
 
@@ -63,19 +78,58 @@ def split_list(alist, wanted_parts=1):
 
 
 def save_data(content):
+    count = 0
+    count_added = 0
+
     content = content.decode("utf-8")
     mystring = content.replace('\n', ' ').replace('\r', '').replace('\t', '')
     soup = BeautifulSoup(mystring, "lxml")
 
     tables = soup.find_all('table')
-    news = [(t, '\n'.join(c[:-2]), c[len(c) - 2][8:], l) for t, c, l in captureText(tables)]
-    allnews.extend(news)
+
+    for id, data in captureText(tables).items():
+        if id not in allnews:
+            allnews[id] = (
+            data[0], '\n'.join(data[1][:-2]), data[1][len(data[1]) - 2][8:], data[2], datetime.datetime.today())
+            count_added += 1
+        else:
+            count += 1
+
+    return count, count_added
 
 
 @app.route('/update', methods=['POST'])
 def update_feed():
-    save_data(request.data)
-    return jsonify({"done": "yes boss"})
+    count, count_added = save_data(request.data)
+    return jsonify({"success": True, "added": count_added, "duplicates": count})
+
+
+@app.route('/testfeed')
+def test_feed():
+    item1 = Item(
+        title="First article",
+        link="http://www.example.com/articles/1",
+        description="This is the description of the first article",
+        author="Santiago L. Valdarrama",
+        guid=Guid("http://www.example.com/articles/1"),
+        pubDate=datetime.datetime(2014, 12, 29, 10, 00))
+
+    item2 = Item(
+        title="Second article",
+        link="http://www.example.com/articles/2",
+        description="This is the description of the second article",
+        author="Santiago L. Valdarrama",
+        guid=Guid("http://www.example.com/articles/2"),
+        pubDate=datetime.datetime(2014, 12, 30, 14, 15))
+
+    feed = Feed(
+        title="Sample RSS Feed",
+        link="http://www.example.com/rss",
+        description="This is an example of how to use rfeed to generate an RSS 2.0 feed",
+        language="en-US",
+        lastBuildDate=datetime.datetime.now(),
+        items=[item1, item2])
+    return feed.rss()
 
 
 @app.route('/')
@@ -83,8 +137,9 @@ def recent_feed():
     feed = AtomFeed('Recent Articles',
                     feed_url=request.url, url=request.url_root)
 
+    print(allnews.values())
     # for each line in the table
-    for i in allnews:
+    for i in allnews.values():
         # getting the identifier
 
 
@@ -92,7 +147,7 @@ def recent_feed():
                  content_type='html',
                  author=i[2],
                  url=make_external(i[3]),
-                 updated=datetime.datetime.today()
+                 updated=i[4]
                  )
 
     return feed.get_response()
